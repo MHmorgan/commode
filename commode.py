1a07990f94d5a1fb240e91ceedf281074cffd97e
Magnus Hirth <mhmorgan42@gmail.com>
2021-06-30 18:31:46 +0200

    Require python3.8 or higher

--- commode.py
+++ commode.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
+
+import sys
+
+if sys.version_info < (3, 8):
+    print('Error: python 3.8 or higher is required. You are currently running:\n%s' % sys.version)
+    sys.exit(1)
 
 if __name__ == '__main__':
     import commode
