622164a59ba9318067224270fe2f54da93a6262f
Magnus Hirth <mhmorgan42@gmail.com>
2021-07-03 19:26:34 +0200

    Print to stderr

--- commode.py
+++ commode.py
@@ -3,7 +3,7 @@
 import sys
 
 if sys.version_info < (3, 8):
-    print('Error: python 3.8 or higher is required. You are currently running:\n%s' % sys.version)
+    print('Error: python 3.8 or higher is required. You are currently running:\n%s' % sys.version, file=sys.stderr)
     sys.exit(1)
 
 if __name__ == '__main__':
