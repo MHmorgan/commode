6cb3fe84f6539139ca93824ef261e664409910c3
Magnus Hirth <mhmorgan42@gmail.com>
2021-07-03 19:26:27 +0200

    Implemented file and directory functionality

--- commode/__init__.py
+++ commode/__init__.py
@@ -1,7 +1,11 @@
 
+from pathlib import Path
+from commode.file_entry import FileEntry
+from commode.server import Server
 import click
-from . import common
-from .common import Error, bail, ctx
+from commode import common
+from commode.common import bail, warn
+from commode.exceptions import Error
 
 
 def run():
@@ -30,14 +34,46 @@ def cli(verbose, quiet, debug, trace, logout, logerr, err2out):  # pylint: disab
     common.LOGOUT = logout
     common.LOGERR = common.LOGOUT if err2out else logerr
 
+    # Server password are stored in plain text in config file - should warn user if 
+    # other users are able to read the config file
+    if common.CONFIG_FILE.stat().st_mode & 0o044:
+        warn(f'config file ({common.CONFIG_FILE}) is readable by group and others')
+
+    # Setup from config
+    cfg = common.CONFIG
+    if (addr := cfg.get('server', 'address', fallback=None)):
+        scheme = cfg.get('server', 'scheme', fallback='https')
+        common.SERVER = Server(address=addr, scheme=scheme)
+    else:
+        warn(f'server address is not configured. Run: `commode config --server-address <addr>`')
+
 
 @cli.command()
-@click.option('--server-address')
-@click.option('--user')
-@click.option('--password')
+@click.option('--server-address', help='Set the address of target Cabinet server')
+@click.option('--user', help='Set username for server authentication')
+@click.option('--password', help='Set password for server authentication')
 def config(server_address, user, password):
-    'Configure the client'
-    # TODO: Configure the client
+    """Configure the client. When no option is provided the current config is printed."""
+    cfg = common.CONFIG
+
+    # Prepare config sections
+    if 'server' not in cfg:
+        cfg.add_section('server')
+
+    if server_address:
+        from urllib.parse import urlparse
+        url = urlparse(server_address)
+        cfg.set('server', 'address', url.netloc)
+        cfg.set('server', 'scheme', url.scheme or 'https')
+    if user:
+        cfg.set('server', 'user', user)
+    if password:
+        cfg.set('server', 'password', password)
+    
+    if server_address or user or password:
+        common.write_config()
+    else:
+        print(common.CONFIG_FILE.read_text(), end='')
 
 
 ################################################################################
@@ -50,7 +86,10 @@ def config(server_address, user, password):
 @click.argument('file')
 def download(file: str):
     'Download a file from the server'
-    # TODO: Download file
+    with common.SERVER:
+        f = FileEntry(file)
+        txt = f.content()
+    print(txt)
 
 
 @cli.command()
@@ -58,14 +97,22 @@ def download(file: str):
 @click.argument('dstfile')
 def upload(srcfile: str, dstfile: str):
     'Upload a file to the server'
-    # TODO: Upload a file
+    try:
+        text = Path(srcfile).read_text()
+    except FileNotFoundError:
+        bail(f'source file not found: {srcfile}')
+    with common.SERVER:
+        f = FileEntry(dstfile)
+        f.update(text)
 
 
 @cli.command()
 @click.argument('file')
 def delete(file: str):
     'Delete a file on the server'
-    # TODO: Delete a file
+    with common.SERVER:
+        f = FileEntry(file)
+        f.delete()
 
 
 ################################################################################
@@ -75,24 +122,28 @@ def delete(file: str):
 ################################################################################
 
 @cli.command()
-@click.argument('path')
+@click.argument('path', required=False, default='')
 def ls(path: str):
     'List directory content on the server'
-    # TODO: ls
+    with common.SERVER as srv:
+        content = srv.get_dir(path)
+    print('\n'.join(sorted(content)))
 
 
 @cli.command()
 @click.argument('path')
 def mkdir(path: str):
     'Create a directory on the server'
-    # TODO: mkdir
+    with common.SERVER as srv:
+        srv.put_dir(path)
 
 
 @cli.command()
 @click.argument('path')
 def rmdir(path: str):
     'Remove a directory on the server'
-    # TODO: rmdir
+    with common.SERVER as srv:
+        srv.delete_dir(path)
 
 
 ################################################################################
